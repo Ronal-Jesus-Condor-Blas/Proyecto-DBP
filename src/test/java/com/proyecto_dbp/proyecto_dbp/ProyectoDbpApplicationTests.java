654cb3a1caa41e5f3a3e2d2687ad175be2c08684
Ronal Jesus <ronal.condor@utec.edu.pe>
2024-10-01 12:29:32 -0500

    Mi primer commit

---
+++ src/test/java/com/proyecto_dbp/proyecto_dbp/ProyectoDbpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto_dbp.proyecto_dbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoDbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
